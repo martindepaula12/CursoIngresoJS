1461c0e23a5f264e43ea6012e40748bc8a3dab26
martindepaula12 <martoondepaula@gmail.com>
2020-07-23 00:12:18 -0300

    ejercisio 11 if

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -4,6 +4,7 @@ function mostrar()
 	var numeroRandom2;
 	var numeroMinimo;
 	var numeroMaximo;
+	var numero3;
 
 	numeroMinimo = 1 // minimo incluido
 	numeroMaximo = 52 // maximo no incluido
@@ -15,6 +16,8 @@ function mostrar()
 	numeroRandom1 = parseInt(numeroRandom1);
 	numeroRandom2 = parseInt(numeroRandom2);
 
+	numero3 = numeroRandom1 + numeroRandom2; // si numero 3 es inpar quiere decir que en los numeros random sumados,  un numero es par y el otro es inpar 
+
 	alert("el numero ramdom 1 es  " + numeroRandom1 + " el numero random 2 es  " + numeroRandom2 );
 
 	if (numeroRandom1 % 2 == 0 && numeroRandom2 % 2 == 0){
@@ -25,12 +28,14 @@ function mostrar()
 	 }
 
 	 if (numeroRandom1 % 5 == 0){ 
-	 	alert("se eleva al cubo " + Math.pow(numeroRandom1, 3))
+	 	alert("numero random 1 elevado al cubo: " + Math.pow(numeroRandom1, 3)) // eleva el numero a la potencia deseada
 	 }
 	 if (numeroRandom2 % 5 == 0){
-		 alert("se eleva al cubo " + Math.pow(numeroRandom2, 3))
+		 alert("numero random 2 elevado al cubo:  " + Math.pow(numeroRandom2, 3))
 	 }
-
+	 if (numero3 % 2 != 0 && numeroRandom1 % 5 != 0 && numeroRandom2 % 5 != 0){
+		 alert("no pasa nada");
+	 }
 
 	 }
 	
