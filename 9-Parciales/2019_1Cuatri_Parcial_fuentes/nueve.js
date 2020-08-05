34575e3f612d0510f55a535defed894169ae115f
martindepaula12 <martoondepaula@gmail.com>
2020-08-05 19:51:27 -0300

    mas ej de parciales

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,92 @@
 function mostrar()
 {
+    var pais;
+    var cantidadHabitantes;
+    var temperatura;
+    var respuesta = "si";
+    var contadorTemperaturasPares = 0;
+    var habitantesDelPaisConMenosHabitantes = 0;
+    var paisConMenosHabitantes;
+    var contadorPaisesConMasDe40Grados = 0;
+    var acumuladorCantidadDeHabitantes = 0;
+    var contadorPaises = 0;
+    var promedioCantidadHabitantes;
+    var primeraVez = 0;
+    var temperaturaMinima;
+    var paisDeTemperaturaMinima;
+    var bandera2 = 0;
+
+    while(respuesta == "si"){
+
+        pais = prompt("ingrese pais");
+
+        while(!(isNaN(pais))){
+            pais = prompt("ingrese un pais correcto");
+        }
+
+        cantidadHabitantes = prompt("ingrese cantidad de habitantes");
+        cantidadHabitantes = parseFloat(cantidadHabitantes);
+
+        while(cantidadHabitantes < 1 || cantidadHabitantes > 7000 || isNaN(cantidadHabitantes)){
+            cantidadHabitantes = prompt("error, ingrese cant de habitantes");
+            cantidadHabitantes = parseFloat(cantidadHabitantes);
+        }
+
+        temperatura = prompt("ingrese temperatura");
+        temperatura = parseFloat(temperatura);
+
+        while(temperatura < -50 || temperatura > 50 || isNaN(temperatura)){
+            temperatura = prompt("ingrese temp correcta");
+            temperatura = parseFloat(temperatura);
+        }
+
+        acumuladorCantidadDeHabitantes = acumuladorCantidadDeHabitantes + cantidadHabitantes;
+        console.log("acum cant hab " + acumuladorCantidadDeHabitantes);
+        contadorPaises++;
+        console.log("cant paises " + contadorPaises);
+
+        if(temperatura % 2 == 0){
+            contadorTemperaturasPares++;
+            console.log("temp pares " + contadorTemperaturasPares);
+        }
+
+        if(bandera2 == 0){
+            bandera2++;
+            habitantesDelPaisConMenosHabitantes = cantidadHabitantes;
+            paisConMenosHabitantes = pais;
+        }
+
+        if(cantidadHabitantes < habitantesDelPaisConMenosHabitantes){
+            habitantesDelPaisConMenosHabitantes = cantidadHabitantes;
+            paisConMenosHabitantes = pais;
+            console.log("pais con men hab " + paisConMenosHabitantes);
+        }
+
+        if(temperatura > 40){
+            contadorPaisesConMasDe40Grados++;
+            console.log("cont paises + 40 grados " + contadorPaisesConMasDe40Grados);
+        }
+
+        if(primeraVez == 0){
+            primeraVez++;
+            temperaturaMinima = temperatura;
+            paisDeTemperaturaMinima = pais;
+        }
+
+        if(temperatura < temperaturaMinima){
+            temperaturaMinima = temperatura;
+            console.log("temp minima " +temperaturaMinima);
+            paisDeTemperaturaMinima = pais;
+            console.log("pais con menos temp "+ paisDeTemperaturaMinima);
+        }
+ 
+
+        respuesta = prompt("desea continuar si/no");
+    }
+    
+    promedioCantidadHabitantes = acumuladorCantidadDeHabitantes / contadorPaises;
+    promedioCantidadHabitantes = promedioCantidadHabitantes.toFixed(2);
+    console.log("prom hab "+ promedioCantidadHabitantes);
+
 
 }
--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,77 @@
 function mostrar()
 {
+    var letra;
+    var numero;
+    var respuesta = "si";
+    var contadorPares = 0;
+    var contadorImpares = 0;
+    var contadorCeros = 0;
+    var sumaNegativos = 0;
+    var primeraVez = 0;
+    var numeroMinimo;
+    var numeroMaximo;
+    var letraMaximo;
+    var letraMinimo;
+
+    while(respuesta == "si"){
+
+        numero = prompt("ingrese numero");
+        numero = parseFloat(numero);
+
+        while(numero < -100 || numero > 100 || isNaN(numero)){
+            numero = prompt("ingrese numero correcto");
+            numero = parseFloat(numero);
+        }
+
+        letra = prompt("ingrese letra");
+
+        while(!(isNaN(letra))){
+            letra = prompt("error, ingrese una letra");
+        }
+
+        if(numero % 2 == 0){
+            contadorPares++;
+            console.log("pares " + contadorPares);
+        }else{ 
+
+        
+            contadorImpares++;
+            console.log("inpares " + contadorImpares);
+            }
+
+        if(numero == 0){
+            contadorCeros++;
+            console.log("ceros " + contadorCeros);
+        }
+
+        if(numero < 0 ){
+            sumaNegativos = sumaNegativos + numero;
+            console.log("sum neg " + sumaNegativos);
+        }
+
+        if(primeraVez == 0){
+            primeraVez++;
+            numeroMaximo = numero;
+            numeroMinimo = numero;
+            letraMaximo = letra;
+            letraMinimo = letra;
+        }
+
+        if(numero > numeroMaximo){
+            numeroMaximo = numero;
+            console.log("num maximo " + numeroMaximo);
+            letraMaximo = letra;
+            console.log("letra max " + letraMaximo);
+        }
+
+        if(numero < numeroMinimo){
+            numeroMinimo = numero;
+            console.log("num minimo " + numeroMinimo);
+            letraMinimo = letra;
+            console.log("letra min " + letraMinimo);
+        }
+
+        respuesta = prompt("desea continuar si/no");
+    }
 
 }
