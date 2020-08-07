9abf5f0ee9eaf4a7d7aa9d8cd1e5593ede2df8fc
martindepaula12 <martoondepaula@gmail.com>
2020-08-07 19:34:32 -0300

    ej parcial 2018

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,79 @@
 function mostrar()
 {
+    var marca;
+    var peso;
+    var temperatura;
+    var respuesta = "si";
+    var temperaturasPares = 0;
+    var pesoMasPesado = 0;
+    var marcaMasPesado;
+    var productosAMenos0Grados = 0;
+    var contadorProductos = 0;
+    var acumuladorPeso = 0;
+    var promedioPeso;
+    var pesoMinimo;
+    var primeraVez = 0;
 
+
+    
+
+    while(respuesta == "si"){
+
+        do
+        {
+            marca = prompt("ingrese marca");
+        }while(!(isNaN(marca)));
+
+        do
+        {
+            peso = prompt("ingrese peso");
+            peso = parseInt(peso);
+        }while(peso < 1 || peso > 100 || isNaN(peso));
+
+        do
+        {
+            temperatura = prompt("ingrese temperatura");
+            temperatura = parseInt(temperatura);
+        }while(temperatura < -30 || temperatura > 30 || isNaN(temperatura));
+
+        if(temperatura % 2 == 0){
+            temperaturasPares++;
+            console.log("temp pares " + temperaturasPares);
+        }
+
+        if(peso > pesoMasPesado){
+            pesoMasPesado = peso;
+            console.log("peso mas pes " + pesoMasPesado);
+            marcaMasPesado = marca;
+            console.log("marca mas pes " + marcaMasPesado);
+        }
+        if(temperatura < 0){
+            productosAMenos0Grados++;
+            console.log("prod a menos 0 "+ productosAMenos0Grados);
+        }
+        if(primeraVez == 0){
+            primeraVez++;
+            pesoMinimo = peso;
+            console.log("peso min " + pesoMinimo);
+        }
+        else if(peso < pesoMinimo){
+            pesoMinimo = peso;
+            console.log("peso min " + pesoMinimo);
+        }
+
+        acumuladorPeso = acumuladorPeso + peso;
+        console.log("acum peso " + acumuladorPeso);
+
+        contadorProductos++;
+        console.log("cont prod " +contadorProductos);
+
+
+        respuesta = prompt("desea continuar si/no");
+    }
+
+    if(contadorProductos != 0){
+        promedioPeso = acumuladorPeso / contadorProductos;
+        promedioPeso = promedioPeso.toFixed(2);
+        console.log("prom peso " + promedioPeso);
+    }
 }
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
@@ -1,4 +1,87 @@
 function mostrar()
 {
-	alert("tres");
-}
+	var respuesta = "si";
+	var edad;
+	var sexo;
+	var temperatura;
+	var nombre;
+	var estadoCivil;
+	var nombreMasTemperatura;
+	var temperaturaMaxima = 0;
+	var viudosMayoresDeEdad = 0;
+	var hombresViudosSolteros = 0;
+	var terceraEdadConMas38Temperatura = 0;
+	var acumuladorEdadHombresSolteros = 0;
+	var contadorHombresSolteros = 0;
+	var promedioEdadHombresSolteros;
+
+	
+
+	while(respuesta == "si"){
+
+		do{
+			nombre = prompt("ingrese nombre")
+		}while(!(isNaN(nombre)));
+
+		do{
+			edad = prompt("ingrese edad")
+			edad = parseInt(edad);
+		}while(edad < 0 || isNaN(edad));
+
+		do{
+			sexo = prompt("ingrese sexo f o m");
+
+		}while(sexo != "f" && sexo != "m");
+
+		do{
+			estadoCivil = prompt("ingrese estado Civil")
+		}while(estadoCivil != "soltero" && estadoCivil != "casado" && estadoCivil != "viudo");
+
+		do{
+			temperatura = prompt("ingrese temperatura");
+			temperatura = parseInt(temperatura);
+		}while(temperatura <= 0 || isNaN(temperatura));
+
+		if (temperatura > temperaturaMaxima){
+			nombreMasTemperatura = nombre;
+			console.log("nomb mas temp " + nombreMasTemperatura);
+			temperaturaMaxima = temperatura;
+			console.log("temp max " + temperaturaMaxima);
+		}
+		
+		if(edad > 17 && estadoCivil == "viudo"){
+			viudosMayoresDeEdad++;
+			console.log("viudos mayores " + viudosMayoresDeEdad);
+		}
+
+		if (sexo == "m" && estadoCivil != "casado"){
+			hombresViudosSolteros++;
+			console.log("hombres viu y sol " + hombresViudosSolteros);
+		}
+
+		if (edad > 60 && temperatura > 38){
+			terceraEdadConMas38Temperatura++;
+			console.log("terc edad + 38 temp " + terceraEdadConMas38Temperatura);
+		}
+
+		if (sexo == "m" && estadoCivil == "soltero"){
+			contadorHombresSolteros++;
+			console.log("cont homb sol " + contadorHombresSolteros);
+			acumuladorEdadHombresSolteros = acumuladorEdadHombresSolteros + edad;
+			console.log("acum edad homb sol " + acumuladorEdadHombresSolteros);
+		}
+
+
+
+
+
+		respuesta = prompt("desea continua si/no");
+	}
+	if(contadorHombresSolteros != 0){
+		promedioEdadHombresSolteros = acumuladorEdadHombresSolteros / contadorHombresSolteros;
+
+		promedioEdadHombresSolteros = promedioEdadHombresSolteros.toFixed(2);
+		console.log("prom edad homb sol " + promedioEdadHombresSolteros);
+	}
+
+	}
